b71ea34045dc983190561c097f7bcd0fc6464d2b
Solange Thomas <solangeathomas@gmail.com>
2021-12-05 19:56:01 -0500

    finished work

--- objectReview_2.js
+++ objectReview_2.js
@@ -11,23 +11,63 @@ let myHotel = {
     selectedGuestRoom: 9999999,
     costOfRooms: [200, 250, 300],
     roomDropDownHTML: "",
-    roomTypes : function(){
+    //roomTypes : function(){
+    //roomStyles : function(){
+    //priceNightly: function(){
+    longStayPrice: function(){
 
-    for(let i = 0; i < this.availableRooms.length; i++){
-        for(let j = 0; j < this.availableRooms[i].length; j++){
+// Make a for loop that outputs all the rooms on a separate line into the console.
+    for (let i = 0; i < this.availableRooms.length; i++){
+        for (let j = 0; j < this.availableRooms[i].length; j++){
             console.log(this.availableRooms[i][j])
         }
     }
-        for(let i = 0; i<this.availableRooms.length; i++){
+
+    //Console log all the rooms with separated by the type of room ("single", "double", "queen")
+    // so that each room is printed still on a separate line, but the room type preceds
+    // the list of the rooms.
+    
+
+        for (let i = 0; i<this.availableRooms.length; i++){
             console.log(this.guestRoomTypeOptions[i])
-        for(let j =0; j< this.availableRooms[i].length; j++){
-            console.log(this.availableRooms[i][j])
+        for (let j =0; j< this.availableRooms[i].length; j++){
+            console.log (this.availableRooms[i][j])
+
         
         }
-            
-}
-    }
+    }      
 
+//Console log the price of the rooms on different lines in a string that says "Room 
+//<ROOM-NUM> costs <COST> per night".
+for (let i = 0; i<this.availableRooms.length; i++){
+    console.log(this.guestRoomTypeOptions[i])
+for (let j =0; j< this.availableRooms[i].length; j++){
+    console.log (`Room ${this.availableRooms[i][j]} costs ${this.costOfRooms[i]} per night.`)
+
+
+}
     
 }
-myHotel.roomTypes();
+//Console log the price of the rooms on different lines in a string that says "Room <ROOM-NUM>
+// is a <ROOM-TYPE> and costs <COST> per night".
+for (let i = 0; i<this.availableRooms.length; i++){
+    console.log(this.guestRoomTypeOptions[i])
+for (let j =0; j< this.availableRooms[i].length; j++){
+    console.log (`Room ${this.availableRooms[i][j]} is a ${this.guestRoomTypeOptions[i]} and costs ${this.costOfRooms[i]} per night.`)
+
+}
+}
+for (let i = 0; i<this.availableRooms.length; i++){
+    console.log(this.guestRoomTypeOptions[i])
+for (let j =0; j< this.availableRooms[i].length; j++){
+     console.log (`Room ${this.availableRooms[i][j]} will cost you ${this.costOfRooms[i] * 13} for 13 nights.`)
+
+}  
+}
+}
+   
+}
+//myHotel.roomTypes();
+//myHotel.roomStyles();
+//myHotel.priceNightly();
+myHotel.longStayPrice();
